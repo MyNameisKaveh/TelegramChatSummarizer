d6e22ac5a52eeaa3bf3fbac5e798941e92dbaaf8
Kaveh <114541229+MyNameisKaveh@users.noreply.github.com>
2025-05-28 03:23:47 +0330

    Update models.py

--- summarizer/models.py
+++ summarizer/models.py
@@ -1,15 +1,7 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 def load_model():
-    model_name = "m3hrdadfi/bert2bert-fa-summarizer"
-
+    model_name = "nafisehNik/mt5-persian-summary"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-
-    summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
-
-    def summarize(text):
-        result = summarizer(text, max_length=128, min_length=30, do_sample=False)
-        return result[0]["summary_text"]
-
-    return summarize
+    return tokenizer, model
