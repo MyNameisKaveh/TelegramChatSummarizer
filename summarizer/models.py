1bd2562521aee8abc07edb087f32817d9c9a2672
Kaveh <114541229+MyNameisKaveh@users.noreply.github.com>
2025-05-28 04:32:17 +0330

    Update models.py

--- summarizer/models.py
+++ summarizer/models.py
@@ -1,8 +1,8 @@
-import os
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 def load_model():
     model_name = "nafisehNik/mt5-persian-summary"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)  # بدون cache_dir
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    return {"model": model, "tokenizer": tokenizer}
+    cache_dir = "/tmp/hf_cache"  # مسیر مجاز برای Hugging Face Spaces
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, cache_dir=cache_dir)
+    return tokenizer, model
