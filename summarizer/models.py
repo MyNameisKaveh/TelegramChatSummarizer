01801c79a486a572669d44bf7fee098d6b5421e0
Kaveh <114541229+MyNameisKaveh@users.noreply.github.com>
2025-05-28 03:31:06 +0330

    Update models.py

--- summarizer/models.py
+++ summarizer/models.py
@@ -1,7 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import os
 
 def load_model():
     model_name = "nafisehNik/mt5-persian-summary"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    cache_dir = "./cache"
+
+    os.makedirs(cache_dir, exist_ok=True)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, cache_dir=cache_dir)
     return tokenizer, model
